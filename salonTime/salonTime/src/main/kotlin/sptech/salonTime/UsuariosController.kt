1d7bdeff87780e879ebdcfc98f624708b16e7a87
Kaua-Navarro <kaua.navarro@sptech.school>
2025-03-11 18:45:23 -0300

    ADIÇÃO DE PATCHS DE (EMAIL, SENHA, COR DO CABELO, COMPRIMENTO DO CABELO)

--- salonTime/salonTime/src/main/kotlin/sptech/salonTime/UsuariosController.kt
+++ salonTime/salonTime/src/main/kotlin/sptech/salonTime/UsuariosController.kt
@@ -1,15 +1,7 @@
 package sptech.salonTime
 
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/usuarios")
@@ -72,7 +64,7 @@ class UsuariosController {
 
     @PutMapping("/{id}")
     fun atualizar(@PathVariable id: Int, @RequestBody usuarioAtualizado: Usuarios): ResponseEntity<String> {
-        if (id < 0 || id >= usuarios.size) {
+        if (id in 0.. usuarios.size-1) {
             return ResponseEntity.status(404).body("Usuário não encontrado")
         }
 
@@ -80,5 +72,39 @@ class UsuariosController {
         return ResponseEntity.ok("Usuário atualizado com sucesso!")
     }
 
+    @PatchMapping("/{id}/{novaSenha}")
+    fun mudarSenha(@PathVariable id:Int, @PathVariable novaSenha:String): ResponseEntity<Usuarios> {
+        if (id in 0..usuarios.size-1) {
+            usuarios[id].senha = novaSenha
+            return ResponseEntity.status(200).body(usuarios[id])
+        }
+        return ResponseEntity.status(404).build()
+    }
 
+    @PatchMapping("/{id}/{novoEmail}")
+    fun mudarEmail(@PathVariable id:Int, @PathVariable novoEmail:String): ResponseEntity<Usuarios> {
+        if (id in 0..usuarios.size-1) {
+            usuarios[id].email = novoEmail
+            return ResponseEntity.status(200).body(usuarios[id])
+        }
+        return ResponseEntity.status(404).build()
+    }
+
+    @PatchMapping("/{id}/{novaCor}")
+    fun mudarCorCabelo(@PathVariable id:Int, @PathVariable novaCor:String): ResponseEntity<Usuarios> {
+        if (id in 0..usuarios.size-1) {
+            usuarios[id].corCabelo = novaCor
+            return ResponseEntity.status(200).body(usuarios[id])
+        }
+        return ResponseEntity.status(404).build()
+    }
+
+    @PatchMapping("/{id}/{novoComprimento}")
+    fun mudarComprimentoCabelo(@PathVariable id:Int, @PathVariable novoComprimento:String): ResponseEntity<Usuarios> {
+        if (id in 0..usuarios.size-1) {
+            usuarios[id].tamanhoCabelo = novoComprimento
+            return ResponseEntity.status(200).body(usuarios[id])
+        }
+        return ResponseEntity.status(404).build()
+    }
 }
