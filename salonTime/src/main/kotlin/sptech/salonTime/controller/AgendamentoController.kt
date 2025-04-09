64d740fff32d694cda154bd1f09db22e84fc7661
elersonleandro <elerson.alves@sptech.school>
2025-04-09 20:11:35 -0300

    Adicionando o método Delete e validação no método POST

--- salonTime/src/main/kotlin/sptech/salonTime/controller/AgendamentoController.kt
+++ salonTime/src/main/kotlin/sptech/salonTime/controller/AgendamentoController.kt
@@ -2,6 +2,7 @@ package sptech.salonTime.controller
 
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -45,12 +46,16 @@ class AgendamentoController(val repository: AgendamentoRepository, val statusAge
 
     @PostMapping
     fun post(@RequestBody @Valid novoAgendamento: Agendamento): ResponseEntity<Agendamento> {
+        val existeAgendamento = repository.existeConflitoDeAgendamento(novoAgendamento.data, novoAgendamento.inicio, novoAgendamento.fim)
+        if (existeAgendamento) {
+            return ResponseEntity.status(400).build()
+        }
         val agendamento = repository.save(novoAgendamento)
         return ResponseEntity.status(201).body(agendamento)
     }
-
-    @PatchMapping("/{atributo}/{id}")
-    fun patch(@PathVariable id: Int, @PathVariable atributo: String, @RequestBody novoValor: String): ResponseEntity<Agendamento> {
+//O campo de fim não pode ser alterado manualmente, pois ele é um cálculo baseado no campo de início e na duração do serviço. O campo de fim é calculado automaticamente com base no horário de início e na duração do serviço associado ao agendamento. Portanto, não é necessário ou apropriado permitir que o usuário altere manualmente esse campo.
+    @PatchMapping("/{atributo}/{id}/{novoValor}")
+    fun patch(@PathVariable id: Int, @PathVariable atributo: String, @PathVariable novoValor:String): ResponseEntity<Agendamento> {
         val agendamento = repository.findById(id).orElse(null)
 
         if (agendamento != null) {
@@ -79,7 +84,18 @@ class AgendamentoController(val repository: AgendamentoRepository, val statusAge
         }
     }
 
+    @GetMapping("proximos")
+    fun getProximosAgendamentos():ResponseEntity<List<Agendamento>>{
+        val agendamentos = repository.buscarProximosAgendamentos()
+        return ResponseEntity.status(201).body(agendamentos)
+    }
 
-
-
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable id: Int): ResponseEntity<Void> {
+        if (repository.existsById(id)){
+            repository.deleteById(id)
+            return ResponseEntity.status(204).build()
+        }
+        return ResponseEntity.status(404).build()
+    }
 }
--- salonTime/src/main/kotlin/sptech/salonTime/repository/AgendamentoRepository.kt
+++ salonTime/src/main/kotlin/sptech/salonTime/repository/AgendamentoRepository.kt
@@ -1,7 +1,23 @@
 package sptech.salonTime.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import sptech.salonTime.entidade.Agendamento
+import java.time.LocalDate
+import java.time.LocalTime
 
 interface AgendamentoRepository: JpaRepository<Agendamento, Int> {
+    @Query(value = "SELECT * FROM agendamento WHERE (data > CURDATE()) OR (data = CURDATE() AND inicio > CURTIME()) ORDER BY data ASC, inicio ASC LIMIT", nativeQuery = true)
+    fun buscarProximosAgendamentos(): List<Agendamento>
+
+    @Query(
+        value = """
+        SELECT COUNT(*) > 0 
+        FROM agendamento 
+        WHERE data = :data 
+          AND ((:inicio BETWEEN inicio AND fim) OR (:fim BETWEEN inicio AND fim) OR (inicio BETWEEN :inicio AND :fim))
+    """,
+        nativeQuery = true
+    )
+    fun existeConflitoDeAgendamento(data: LocalDate?, inicio: LocalTime?, fim: LocalTime?): Boolean
 }
